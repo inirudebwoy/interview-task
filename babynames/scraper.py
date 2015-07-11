83522bcdf02681f7beb7217405d5ad31f2f7100f
Flyte <flyte@failcode.co.uk>
2015-07-11 17:17:28 +0100

    Write tests for and wire up get() method in scraper.

--- babynames/scraper.py
+++ babynames/scraper.py
@@ -3,8 +3,34 @@ from bs4 import BeautifulSoup
 import re
 
 
-def get(year):
+class NoNamesTableError(Exception):
     pass
+
+
+class IncorrectAmountOfTDElementsError(Exception):
+    pass
+
+
+class TDElementsOutOfOrderError(Exception):
+    pass
+
+
+def get(year, url):
+    """
+    Performs a request to the URL provided, parses the response and returns a tuple containing
+    two dictionaries - one for boys and one for girls names. The dictionaries have the name as the
+    key and the rank as the value.
+    """
+    req = _create_request(year, url)
+    resp = _perform_request(req)
+    bs = _parse_response(resp)
+    names = _extract_names_table(bs)
+    boys, girls = {}, {}
+    for tr in _extract_name_rows(names):
+        rank, boy, girl = _extract_names_from_row(tr)
+        boys[boy] = rank
+        girls[girl] = rank
+    return boys, girls
 
 
 def _create_request(year, url, top=1000):
@@ -36,4 +62,40 @@ def _extract_names_table(bs):
     for table in bs.findAll("table"):
         if re.match(r"^Popularity for top \d+$", table.attrs.get("summary", "")):
             return table
-    raise ValueError("Unable to find the names table in the given BeautifulSoup object")
+    raise NoNamesTableError("Unable to find the names table in the given BeautifulSoup object")
+
+
+def _extract_name_rows(table):
+    """
+    Removes the first and last tr from the table and returns a list of trs.
+    """
+    return table.findAll("tr")[1:-1]
+
+
+def _extract_names_from_row(tr):
+    """
+    Returns a tuple containing the boy's and girl's name as separate elements
+    when given a BeautifulSoup tr in the correct format.
+
+    eg. '<tr align="right"><td>786</td><td>Ellis</td><td>Kaylah</td></tr>'
+    """
+    tds = tr.findAll("td")
+    if len(tds) != 3:
+        raise IncorrectAmountOfTDElementsError(
+            "Expecting the tr to contain three td elements but it contained %s" % len(tds))
+    try:
+        rank = int(tds[0].text)
+    except ValueError:
+        raise TDElementsOutOfOrderError(
+            "The first td element was not a rank number: %s" % tds[0].text)
+    names = tuple([t.text for t in tds][1:])
+    for name in names:
+        try:
+            int(name)
+        except ValueError:
+            # Good, it's not a number!
+            continue
+        raise TDElementsOutOfOrderError(
+            "TD elements must have been in the wrong order as '%s'"" is not any name I've heard of"
+            % name)
+    return (rank,) + names
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -29,12 +29,6 @@ class TestScraper:
                 self._example_html = codecs.encode(f.read())
         return self._example_html
 
-    def test_get_takes_year_arg(self):
-        """
-        get() should take a 'year' argument.
-        """
-        scraper.get(year=2000)
-
     def test_create_request_takes_year_and_url_args(self):
         """
         _create_request() should take the 'year' and 'url' arguments.
@@ -105,7 +99,7 @@ class TestScraper:
 
     def test_parse_response_returns_beautifulsoup_object(self):
         """
-        _parse_response should return a BeautifulSoup object.
+        _parse_response() should return a BeautifulSoup object.
         """
         resp = Response()
         resp._content = self.example_html
@@ -114,7 +108,7 @@ class TestScraper:
 
     def test_extract_names_table_returns_table(self):
         """
-        _extract_names_table should return the correct BeautifulSoup table element.
+        _extract_names_table() should return the correct BeautifulSoup table element.
         """
         resp = Response()
         resp._content = self.example_html
@@ -125,7 +119,88 @@ class TestScraper:
 
     def test_extract_names_table_raises_exception_when_table_doesnt_exist(self):
         """
-        _extract_names_table should raise a ValueError if it can't find the names table.
+        _extract_names_table() should raise a NoNamesTableError if it can't find the names table.
         """
-        with pytest.raises(ValueError):
+        with pytest.raises(scraper.NoNamesTableError):
             scraper._extract_names_table(BeautifulSoup())
+
+    def test_extract_names_from_row(self):
+        """
+        _extract_names_from_row() should return a tuple of (<rank>, <boy_name>, <girl_name>) when
+        given a BeautifulSoup 'tr' object.
+        """
+        tr_html = '<tr align="right"><td>786</td><td>Ellis</td><td>Kaylah</td></tr>'
+        tr = BeautifulSoup(tr_html)
+        rank, boy, girl = scraper._extract_names_from_row(tr)
+        assert rank == 786
+        assert boy == "Ellis"
+        assert girl == "Kaylah"
+
+    def test_extract_names_from_row_raises_exception_when_not_enough_tds(self):
+        """
+        _extract_names_from_row() should raise a IncorrectAmountOfTDElementsError if there are less
+        than three td elements.
+        """
+        tr_html = '<tr align="right"><td>Ellis</td><td>Kaylah</td></tr>'
+        tr = BeautifulSoup(tr_html)
+        with pytest.raises(scraper.IncorrectAmountOfTDElementsError):
+            scraper._extract_names_from_row(tr)
+
+    def test_extract_names_from_row_raises_exception_when_too_many_tds(self):
+        """
+        _extract_names_from_row() should raise a IncorrectAmountOfTDElementsError if there are more
+        than three td elements.
+        """
+        tr_html = '<tr align="right"><td>786</td><td>Ellis</td><td>Kaylah</td><td>foo</td></tr>'
+        tr = BeautifulSoup(tr_html)
+        with pytest.raises(scraper.IncorrectAmountOfTDElementsError):
+            scraper._extract_names_from_row(tr)
+
+    def test_extract_names_from_row_raises_exception_when_tds_are_out_of_order(self):
+        """
+        _extract_names_from_row() should raise a TDElementsOutOfOrderError if the tds are in a
+        different order to what it expects and it would otherwise return a number as a name.
+        """
+        tr_html = '<tr align="right"><td>Ellis</td><td>786</td><td>Kaylah</td></tr>'
+        tr = BeautifulSoup(tr_html)
+        with pytest.raises(scraper.TDElementsOutOfOrderError):
+            scraper._extract_names_from_row(tr)
+
+    def test_get_returns_tuple_of_dicts_for_boy_and_girl(self):
+        """
+        get() should return a tuple containing a dictionary for boy's and girl's names
+        respectively. The dicts should have the names as keys and their rank as values.
+        """
+        fake_resp = Response()
+        fake_resp._content = self.example_html
+        with patch.object(Session, "send", return_value=fake_resp):
+            boys, girls = scraper.get(year=2000, url=LOCAL_URL)
+        assert isinstance(boys, dict)
+        assert isinstance(girls, dict)
+        assert "Ellis" in boys
+        assert "Yolanda" in girls
+        assert boys["Ellis"] == 786
+        assert girls["Yolanda"] == 780
+
+    def test_extract_name_rows(self):
+        """
+        _extract_name_rows() should return only the name trs from the given table. Essentially
+        just removing the first and last trs.
+        """
+        html = """
+        <html>
+         <body>
+          <table>
+           <tr><th>This is a header row</th></tr>
+           <tr><td>1</td><td>Ellis</td><td>Sarah</td></tr>
+           <tr><td>2</td><td>Michal</td><td>Lucy</td></tr>
+           <tr><td>This is a footer row</td></tr>
+          </table>
+         </body>
+        </html>
+        """
+        table = BeautifulSoup(html)
+        rows = scraper._extract_name_rows(table)
+        assert len(rows) == 2
+        for row in rows:
+            assert not any(("header row" in row.text, "footer row" in row.text))
