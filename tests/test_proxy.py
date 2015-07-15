139fb79f1f8bea78d27a61c83194014042d9d8e0
Michael Klich <michal@michalklich.com>
2015-07-15 23:24:58 +0100

    Add few tests

---
+++ tests/test_proxy.py
@@ -0,0 +1,15 @@
+class ProxyGetTests:
+    pass
+
+
+class ScraperCommunicationTests:
+    def test_get_returns_tuple_of_dicts():
+        pass
+
+
+class CacheCommunicationTests:
+    def test_get_from_cache_returns_empty_tuple():
+        pass
+
+    def test_get_from_cache_returns_tuple_with_data():
+        pass
