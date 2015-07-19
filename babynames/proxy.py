ec67e41d47b8f35fae596b0bcb56e77663b76a40
Michael Klich <michal@michalklich.com>
2015-07-19 22:06:29 +0100

    TDD first part, tests are ready

---
+++ babynames/proxy.py
@@ -0,0 +1,13 @@
+from babynames import scraper
+
+
+def get(year, url):
+    pass
+
+
+def _get_from_web(year, url):
+    pass
+
+
+def _get_from_cache(year, url):
+    pass
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,15 +1,49 @@
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from babynames import proxy
+
+
+EMPTY_RESPONSE = ({}, {})
+VALID_RESPONSE = ({'Michael': 7}, {'Emily': 7})
+
+
 class TestProxyGet:
-    pass
+    @patch('babynames.proxy.scraper.get', return_value=EMPTY_RESPONSE)
+    def test_get_returns_tuple_of_empty_dicts(self, scraper_mock):
+        assert proxy.get(2014, 'www.url.com') == ({}, {})
+
+    @patch('babynames.proxy.scraper.get', return_value=VALID_RESPONSE)
+    def test_get_returns_tuple_of_dicts_with_data(self, scraper_mock):
+        dict_one, dict_two = proxy.get(2014, 'www.url.com')
+        for name, rank in dict_one.update(dict_two):
+            assert isinstance(name, str)
+            assert isinstance(rank, int)
 
 
 class TestScraperCommunication:
-    def test_get_returns_tuple_of_dicts(self):
-        pass
+    @patch('babynames.proxy.scraper.get', return_value=EMPTY_RESPONSE)
+    def test_get_from_web_returns_tuple_of_empty_dicts(self, scraper_mock):
+        assert proxy._get_from_web(2014, 'www.url.com') == ({}, {})
+
+    @patch('babynames.proxy.scraper.get', return_value=VALID_RESPONSE)
+    def test_get_from_web_returns_tuple_with_valid_data(self, scraper_mock):
+        dict_one, dict_two = proxy._get_from_web(2014, 'www.url.com')
+        for name, rank in dict_one.update(dict_two):
+            assert isinstance(name, str)
+            assert isinstance(rank, int)
 
 
 class TestCacheCommunication:
-    def test_get_from_cache_returns_empty_tuple(self):
-        pass
+    @patch('babynames.proxy.scraper.get', return_value=EMPTY_RESPONSE)
+    def test_get_from_cache_returns_empty_tuple(self, scraper_mock):
+        assert proxy._get_from_cache(2014, 'www.url.com') == ({}, {})
 
-    def test_get_from_cache_returns_tuple_with_data(self):
-        pass
+    @patch('babynames.proxy.scraper.get', return_value=VALID_RESPONSE)
+    def test_get_from_cache_returns_tuple_with_valid_data(self, scraper_mock):
+        dict_one, dict_two = proxy._get_from_cache(2014, 'www.url.com')
+        for name, rank in dict_one.update(dict_two):
+            assert isinstance(name, str)
+            assert isinstance(rank, int)
