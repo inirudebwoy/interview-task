b9da9b4842d4294d04e79f0c80fc401eb46f26d9
Michael Klich <michal@michalklich.com>
2015-07-31 17:03:11 +0100

    Add proxy with tests

--- babynames/proxy.py
+++ babynames/proxy.py
@@ -1,15 +1,20 @@
+import os
+import json
+
 from babynames import scraper
 
-CACHE_DIR = '_cache'
+CACHE_DIR = os.path.join(os.path.abspath('.'), '_cache')
+CACHE_FILENAME_FORMAT = '%s.txt'
+EMPTY_CACHE = ({}, {})
 
 
 def get(year, url):
-    cached_value = _get_from_cache(year, url)
+    cached_value = _get_from_cache(year)
     if any(cached_value):
         return cached_value
 
     web_value = _get_from_web(year, url)
-    _save_to_cache(web_value)
+    _save_to_cache(year, web_value)
     return web_value
 
 
@@ -17,9 +22,20 @@ def _get_from_web(year, url):
     return scraper.get(year, url)
 
 
-def _get_from_cache(year, url):
-    pass
+def _get_from_cache(year):
+    cache_file_path = os.path.join(CACHE_DIR, CACHE_FILENAME_FORMAT % year)
+    if os.path.exists(cache_file_path):
+        cache_file = open(cache_file_path)
+        return json.load(cache_file)
+    return EMPTY_CACHE
 
 
-def _save_to_cache(data):
-    pass
+def _save_to_cache(year, data):
+    # don't save empty data
+    if not any(data):
+        return
+    if not os.path.exists(CACHE_DIR):
+        os.mkdir(CACHE_DIR)
+    cache_file_path = os.path.join(CACHE_DIR, CACHE_FILENAME_FORMAT % year)
+    cache_file = open(cache_file_path, 'w')
+    json.dump(data, cache_file)
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+import json
 try:
     from unittest.mock import patch
 except ImportError:
@@ -5,51 +8,85 @@ except ImportError:
 
 from babynames import proxy
 
+import pytest
 
 EMPTY_RESPONSE = ({}, {})
 VALID_RESPONSE = ({'Michael': 7}, {'Emily': 7})
+YEAR = 2014
+
+
+@pytest.fixture
+def clear_cache():
+    if os.path.exists(proxy.CACHE_DIR):
+        shutil.rmtree(proxy.CACHE_DIR)
+
+
+@pytest.fixture
+def set_cache():
+    if not os.path.exists(proxy.CACHE_DIR):
+        os.mkdir(proxy.CACHE_DIR)
+    cache_file_path = os.path.join(proxy.CACHE_DIR,
+                                   proxy.CACHE_FILENAME_FORMAT % YEAR)
+    cache_file = open(cache_file_path, 'w')
+    json.dump(VALID_RESPONSE, cache_file)
+    cache_file.close()
 
 
 class TestProxyGet:
     @patch('babynames.proxy.scraper.get', return_value=EMPTY_RESPONSE)
-    def test_get_returns_tuple_of_empty_dicts(self, scraper_mock):
-        assert proxy.get(2014, 'www.url.com') == ({}, {})
+    @patch('babynames.proxy._get_from_cache', return_value=EMPTY_RESPONSE)
+    def test_get_returns_tuple_of_empty_dicts(self, scraper_mock, cache_mock):
+        assert proxy.get(YEAR, 'www.url.com') == EMPTY_RESPONSE
 
     @patch('babynames.proxy.scraper.get', return_value=VALID_RESPONSE)
-    def test_get_returns_tuple_of_dicts_with_data(self, scraper_mock):
-        dict_one, dict_two = proxy.get(2014, 'www.url.com')
-        for name, rank in dict_one.update(dict_two):
-            assert isinstance(name, str)
-            assert isinstance(rank, int)
+    @patch('babynames.proxy._get_from_cache', return_value=VALID_RESPONSE)
+    def test_get_returns_tuple_of_dicts_with_data(self, scraper_mock,
+                                                  cache_mock):
+        dict_one, dict_two = proxy.get(YEAR, 'www.url.com')
+        assert isinstance(dict_one, dict)
+        assert isinstance(dict_two, dict)
+        assert dict_one
+        assert dict_two
 
 
 class TestScraperCommunication:
     @patch('babynames.proxy.scraper.get', return_value=EMPTY_RESPONSE)
     def test_get_from_web_returns_tuple_of_empty_dicts(self, scraper_mock):
-        assert proxy._get_from_web(2014, 'www.url.com') == ({}, {})
+        assert proxy._get_from_web(YEAR, 'www.url.com') == EMPTY_RESPONSE
 
     @patch('babynames.proxy.scraper.get', return_value=VALID_RESPONSE)
     def test_get_from_web_returns_tuple_with_valid_data(self, scraper_mock):
-        dict_one, dict_two = proxy._get_from_web(2014, 'www.url.com')
-        for name, rank in dict_one.update(dict_two):
-            assert isinstance(name, str)
-            assert isinstance(rank, int)
+        dict_one, dict_two = proxy._get_from_web(YEAR, 'www.url.com')
+        assert isinstance(dict_one, dict)
+        assert isinstance(dict_two, dict)
+        assert dict_one
+        assert dict_two
 
 
 class TestCacheCommunication:
-    @patch('babynames.proxy.scraper.get', return_value=EMPTY_RESPONSE)
-    def test_get_from_cache_returns_empty_tuple(self, scraper_mock):
-        assert proxy._get_from_cache(2014, 'www.url.com') == ({}, {})
+    def test_get_from_cache_returns_empty_tuple(self, clear_cache):
+        assert proxy._get_from_cache(YEAR) == EMPTY_RESPONSE
 
-    @patch('babynames.proxy.scraper.get', return_value=VALID_RESPONSE)
-    def test_get_from_cache_returns_tuple_with_valid_data(self, scraper_mock):
-        dict_one, dict_two = proxy._get_from_cache(2014, 'www.url.com')
-        for name, rank in dict_one.update(dict_two):
-            assert isinstance(name, str)
-            assert isinstance(rank, int)
+    def test_get_from_cache_returns_tuple_with_valid_data(self, set_cache):
+        dict_one, dict_two = proxy._get_from_cache(YEAR)
+        assert isinstance(dict_one, dict)
+        assert isinstance(dict_two, dict)
+        assert dict_one
+        assert dict_two
 
-    def test_save_to_cache_no_cache_dir(self):
-        pass
 
-    def test_save_to_cache_with_cache_dir(self):
-        pass
+class TestCacheOperations:
+    def test_save_to_cache_no_cache_dir(self, clear_cache):
+        cache_file_path = os.path.join(proxy.CACHE_DIR,
+                                       proxy.CACHE_FILENAME_FORMAT % YEAR)
+
+        proxy._save_to_cache(YEAR, VALID_RESPONSE)
+        assert os.path.exists(cache_file_path)
+
+    def test_save_to_cache_with_cache_dir(self, clear_cache):
+        os.mkdir(proxy.CACHE_DIR)
+        cache_file_path = os.path.join(proxy.CACHE_DIR,
+                                       proxy.CACHE_FILENAME_FORMAT % YEAR)
+
+        proxy._save_to_cache(YEAR, VALID_RESPONSE)
+        assert os.path.exists(cache_file_path)
