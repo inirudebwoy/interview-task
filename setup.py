4f103b375d9b272d849a375f5b0ccaeed070524c
Michael Klich <michal@michalklich.com>
2015-07-31 17:18:54 +0100

    Add bumpversion, simplify setup

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from os.path import join, dirname
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+from setuptools import setup
 
 
 def read(fname):
